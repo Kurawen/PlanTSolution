b54789b3328812598df0d4f808f8353abbd79eaf
dimekadze <newakk74@mail.ru>
2025-11-05 16:56:38 +0500

    v4.3.2

--- App/Services/PasswordHasher.cs
+++ App/Services/PasswordHasher.cs
@@ -1,11 +1,11 @@
-﻿using BCrypt.Net;
+﻿// using BCrypt.Net;
 
-namespace App.Services;
-public class PasswordHasher : IPasswordHasher
-{
-    public string GenerateHashPassword(string password) =>
-        BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+// namespace App.Services;
+// public class PasswordHasher
+// {
+//     public string GenerateHashPassword(string password) =>
+//         BCrypt.Net.BCrypt.EnhancedHashPassword(password);
 
-    public bool VerifyPassword(string password, string hashedPassword) =>
-        BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
-}
+//     public bool VerifyPassword(string password, string hashedPassword) =>
+//         BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
+// }
--- App/Services/UserService.cs
+++ App/Services/UserService.cs
@@ -1,32 +1,32 @@
-namespace App.Services;
+// namespace App.Services;
 
-public class UserService
-{
-    private readonly IPasswordHasher _passwordHasher;
-    private readonly IUsersRepository _usersRepository;
+// public class UserService
+// {
+//     private readonly IPasswordHasher _passwordHasher;
+//     // private readonly IUsersRepository _usersRepository;
 
 
-    public UserService(IUsersRepository usersRepository, IPasswordHasher passwordHasher)
-    {
-        _passwordHasher = passwordHasher;
-        _passwordHasher = passwordHasher;
-    }
-    // регистрация аккаунта
-    public async Task Register(string userName, string email, string password)
-    {
-        var hashedPassword = _passwordHasher.Generate(password);
+//     public UserService(IUsersRepository usersRepository, IPasswordHasher passwordHasher)
+//     {
+//         _passwordHasher = passwordHasher;
+//         _passwordHasher = passwordHasher;
+//     }
+//     // регистрация аккаунта
+//     public async Task Register(string userName, string email, string password)
+//     {
+//         var hashedPassword = _passwordHasher.Generate(password);
 
-        var user = User.Create(Guid.NewGuid(), userName, hashedPassword, email);
+//         var user = User.Create(Guid.NewGuid(), userName, hashedPassword, email);
 
-        await _userRepository.Add(user);
-    }
+//         await _userRepository.Add(user);
+//     }
 
-    // вход в аккаунт(логин)
-    public async Task<string> Login(string email, string password)
-    {
-        var token = await usersService.Login();
+//     // вход в аккаунт(логин)
+//     public async Task<string> Login(string email, string password)
+//     {
+//         var token = await usersService.Login();
         
 
-        return;
-    }
-}
+//         return;
+//     }
+// }
